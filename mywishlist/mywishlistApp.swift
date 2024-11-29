71cbe210fa52427a99fbd25e34986cfaf1ab1fbf
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-29 22:24:48 +0100

    Initial Commit

---
+++ mywishlist/mywishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  mywishlistApp.swift
+//  mywishlist
+//
+//  Created by Jean on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct mywishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
